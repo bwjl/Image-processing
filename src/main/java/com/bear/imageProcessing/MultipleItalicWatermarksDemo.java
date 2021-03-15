320d1f3b25085d32fee02c528ca27d25c7409dd7
bear <593788753@qq.com>
2021-03-15 22:07:56 +0800

    init

---
+++ src/main/java/com/bear/imageProcessing/MultipleItalicWatermarksDemo.java
@@ -0,0 +1,12 @@
+package com.bear.imageProcessing;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: bear
+ * @Date: 2021/3/15 10:07 下午
+ * @Description: 多个斜体水印
+ */
+
+public class MultipleItalicWatermarksDemo {
+}
